9b09985d3eb6ba8dc163ec05f4ab5340d316f865
loicbefve <befve.loic@gmail.com>
2017-10-17 20:03:39 +0200

    Fonction Inversion
    La fonction inversion est implémentée et fonctionelle

--- src/ImageViewer.java
+++ src/ImageViewer.java
@@ -6,36 +6,50 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.BoxLayout;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
 public class ImageViewer extends JFrame /*implements ActionListener*/
 {
 	private DisplayedImage inputImage = new DisplayedImage(); 
 	private DisplayedImage ouputImage = new DisplayedImage();
 	private JButton buttonAction = new JButton("Action");
+	private JButton buttonInversion = new JButton("Inversion");
 
 	private JMenuBar menuBar = new JMenuBar();
 	private JMenu fileMenu = new JMenu("File");
 
 	private JMenuItem itemClose = new JMenuItem("Close");
+	
+	private JPanel output = new JPanel();
 
 	public ImageViewer () {
 		this.setTitle("Image Viewer");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(1000, 400);
-
+		
+		//Image d'entrée
 		JPanel input = new JPanel();
 		input.setLayout(new BoxLayout(input, BoxLayout.PAGE_AXIS));
 		input.add(inputImage);
-
+		
+		//Bouton1
 		JPanel action = new JPanel();
 		action.setLayout(new BoxLayout(action, BoxLayout.PAGE_AXIS));
 		action.add(buttonAction);
+		
+		//BoutonInversion
+		JPanel inversion = new JPanel();
+		inversion.setLayout(new BoxLayout(inversion, BoxLayout.PAGE_AXIS));
+		inversion.add(buttonInversion);
+		
 		// Defines action associated to buttons
 		buttonAction.addActionListener(new ButtonListener());
-
-		JPanel output = new JPanel();
+		buttonInversion.addActionListener(new InversionListener());
+		
+		//Image de sortie
 		output.setLayout(new BoxLayout(output, BoxLayout.PAGE_AXIS));
 		output.add(ouputImage); 
 
@@ -43,6 +57,7 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 		global.setLayout(new BoxLayout(global, BoxLayout.LINE_AXIS));
 		global.add(input);
 		global.add(action);
+		global.add(inversion);
 		global.add(output);
 
 		this.getContentPane().add(global);
@@ -70,4 +85,48 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 			System.out.println("Action Performed");
 		}
 	}
+	
+	class InversionListener implements ActionListener{
+		public void actionPerformed(ActionEvent arg0)
+		{
+			BufferedImage imageInput = inputImage.image;
+			BufferedImage imageOutput = imageInput;
+			
+			int width = imageInput.getWidth();
+			int height = imageInput.getHeight();
+			
+			for (int i = 0; i < width; i++) {
+		        for (int j = 0; j < height; j++) {
+		         
+		        	//Je récupère la couleur de chaque pixel
+		            Color pixelcolor= new Color(imageInput.getRGB(i, j));
+		             
+		            //Je récupère chaque composante
+		            int r=pixelcolor.getRed();
+		            int g=pixelcolor.getGreen();
+		            int b=pixelcolor.getBlue();
+		             
+		            //J'inverse les couleurs
+		            r=Math.abs(r-255);
+		            g=Math.abs(g-255);
+		            b=Math.abs(b-255);
+		            
+		            //Je repasse en int
+		            Color rgbNew = new Color(r,g,b);
+		            int rgb = rgbNew.getRGB();
+		             
+		            //Je modifie l'output
+		            imageOutput.setRGB(i, j, rgb);
+		            
+		            ouputImage.image = imageOutput;
+		            output.repaint();
+		            		            
+		            
+		 
+		        }
+		    }
+		}
+	}
+	
+
 }
