af7b6c32e8b4cfe11f6b3a5c73e0c10e2794e1e8
loicbefve <befve.loic@gmail.com>
2017-10-23 01:36:35 +0200

    KdTree fonctionnel, algorithme de recherche en cours

--- src/App.java
+++ src/App.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+import kdtree.*;
 /**
  * Basic image processing application
  *
@@ -9,7 +12,19 @@ public class App
     {
 		@SuppressWarnings("unused")
 		ImageViewer mainWindow = new ImageViewer();
+    
+    
+    Point p1 = new Point(1,2);
+	Point p2 = new Point(1,3);
+	Point p3 = new Point(1,7);
+	Point p4 = new Point(1,3);
+	List<Point> li = new ArrayList<Point>();
+	li.add(p1);
+	li.add(p2);
+	li.add(p3);
+	li.add(p4);
+	KdTree tree = new KdTree( li , 2 , 0);
+	boolean rech = tree.recherche( 7 , 1);
+	System.out.println(rech);
     }
-    
-    
 }
--- src/kdtree/KdTree.java
+++ src/kdtree/KdTree.java
@@ -1,12 +1,13 @@
 package kdtree;
+import java.util.ArrayList;
 import java.util.List;
 
 public class KdTree {
 	
 	private class KdNode {
 			
-			private KdNode filsDroit = null;
-			private KdNode filsGauche = null;
+			private KdNode filsDroit;
+			private KdNode filsGauche;
 			@SuppressWarnings("unused")
 			private int direction;
 			@SuppressWarnings("unused")
@@ -45,42 +46,65 @@ public class KdTree {
 			
 		}
 	
-	@SuppressWarnings("unused")
-	private KdNode racine;
+	public KdNode racine;
 
-//TODO: Terminer l'implémentation de la fonction de tri pour passer par la médiane
-//	//Tri de la liste par tri/fusion
-//	private Point[] tri( Point listePoints[] , int direction ) {
-//		int size = listePoints.length;
-//		if( size <= 1) {
-//			return listePoints;
-//		}
-//		else {
-//			Point listeGauche[];
-//			Point listeDroit[];
-//			for( int i=0 ; i<size ; i++) {
-//				if(i<= size/2) {
-//					listeGauche[i] = listePoints[i];
-//				}
-//				else {
-//					listeDroit[i-size/2] = listePoints[i];
-//				}
-//			}
-//			return fusion( tri(listeGauche) , tri(listeDroit) , direction);
-//		}
-//	}
-//	private Point[] fusion(Point listeA[], Point listeB[] , int direction) {
-//		
-//		if( listeA.length == 0 ) {
-//			return listeB;
-//		}
-//		if( listeB.length == 0 ) {
-//			return listeA;
-//		}
-//		if( listeA[1].coord[direction] <= listeB[1].coord[direction] ) {
-//			return 
-//		}
-//	}
+
+	//Tri de la liste par tri/fusion
+	private List<Point> tri( List<Point> listePoints , int direction ) {
+		int size = listePoints.size();
+		if( size <= 1) {
+			return listePoints;
+		}
+		else {
+			List<Point> listeGauche = listePoints.subList(0, size/2-1);
+			List<Point> listeDroite = listePoints.subList(size/2, size-1);
+			return fusion( tri(listeGauche , direction) , tri(listeDroite , direction) , direction);
+		}
+	}
+	private List<Point> fusion(List<Point> listeA, List<Point> listeB , int direction) {
+		
+		if( listeA == null ) {
+			return listeB;
+		}
+		if( listeB == null ) {
+			return listeA;
+		}
+		if( listeA.get(0).coord[direction] <= listeB.get(0).coord[direction] ) {
+			List<Point> ret = new ArrayList<Point>();
+			ret.add(listeA.get(0));
+			if(listeA.size() > 1) {
+				ret.addAll(fusion(listeA.subList(1, listeA.size()-1) , listeB , direction));
+			}
+			else {
+				ret.addAll(fusion( null , listeB , direction));
+			}
+			
+			return ret;
+		}
+		else {
+			List<Point> ret = new ArrayList<Point>();
+			ret.add(listeB.get(0));
+			if(listeB.size() > 1) {
+				ret.addAll(fusion(listeA , listeB.subList(1, listeB.size()-1) , direction));
+			}
+			else {
+				ret.addAll(fusion(listeA , null , direction));
+			}
+			return ret;
+		}
+	}
+	
+	private Point mediane( List<Point> listePointsTriee , int direction) {
+		if(listePointsTriee.size() > 0) {
+			if(listePointsTriee.size()%2 != 0) {
+				return listePointsTriee.get(listePointsTriee.size()/2);
+			}
+			return listePointsTriee.get(listePointsTriee.size()/2-1);
+		}
+		else {
+			return listePointsTriee.get(0);
+		}
+	}
 	
 	private KdNode createKdTree (List<Point> listePoints , int k , int profondeur) {
 		if( listePoints.isEmpty() ) {
@@ -89,12 +113,28 @@ public class KdTree {
 		
 		int direction = profondeur%k;
 		
-		int size = listePoints.size();
-		Point point = listePoints.get(size/2);
-		KdNode filsGauche = createKdTree(listePoints.subList(0, size/2) , k , profondeur+1);
-		KdNode filsDroit = createKdTree(listePoints.subList(size/2+1 , size) , k , profondeur+1);
-		KdNode res = new KdNode( filsGauche, filsDroit, point, direction);
-		return res;
+		List<Point> listePointsTriee = tri(listePoints , direction);
+		int size = listePointsTriee.size();
+		
+		Point point = mediane(listePointsTriee , direction);
+		if(listePointsTriee.size() > 2) {
+			KdNode filsGauche = createKdTree(listePointsTriee.subList(0, listePointsTriee.indexOf(point)-1) , k , profondeur+1);
+			KdNode filsDroit = createKdTree(listePointsTriee.subList(listePointsTriee.indexOf(point)+1 , size-1) , k , profondeur+1);
+			KdNode res = new KdNode( filsGauche, filsDroit, point, direction);
+			return res;
+		}
+		else if (listePointsTriee.size() == 2){
+			KdNode filsGauche = null;
+			KdNode filsDroit = createKdTree(listePointsTriee.subList(listePointsTriee.indexOf(point)+1 , size-1) , k , profondeur+1);
+			KdNode res = new KdNode( filsGauche, filsDroit, point, direction);
+			return res;
+		}
+		else {
+			KdNode filsDroit = null;
+			KdNode filsGauche = null;
+			KdNode res = new KdNode( filsGauche, filsDroit, point, direction);
+			return res;
+		}
 	}
 	
 	
@@ -102,19 +142,39 @@ public class KdTree {
 		this.racine = createKdTree( listePoints , k , profondeur );
 	}
 	
-	public void recherche ( KdNode noeud ) {
-		
+	private KdNode algoRecherche( KdNode noeudDepart, int coordCherch , int direction) {
+		if( noeudDepart.point.coord[direction] == coordCherch ) {
+			return noeudDepart;
+		}
+		else if(coordCherch > noeudDepart.point.coord[direction] && noeudDepart.filsDroit != null) {
+			System.out.println("gyg1");
+			algoRecherche(noeudDepart.filsDroit , coordCherch , direction);
+		}
+		else if(coordCherch < noeudDepart.point.coord[direction] && noeudDepart.filsGauche != null) {
+			System.out.println("gy2");
+			algoRecherche(noeudDepart.filsGauche , coordCherch , direction);
+		}
+		else if(noeudDepart.isTerminal() ) {
+			System.out.println("gyg");
+			return null;
+		}
+		System.out.println("Cas non géré");
+		return null;
 	}
-	public void addNode( Point point ) {
+	
+	public boolean recherche(int coordCherch , int direction) {
+		return (algoRecherche(this.racine , coordCherch, direction)!=null);
 	}
+//	public void addNode( Point point ) {
+//	}
 	
 	// TODO : Plus tard
 //	public void removeNode ( Point point ) {
 //		
 //	}
 	
-	public KdNode getNearestNeighbor( Point point ) {
-		return null;
-	}
+//	public KdNode getNearestNeighbor( Point point ) {
+//		return null;
+//	}
 	
 }
