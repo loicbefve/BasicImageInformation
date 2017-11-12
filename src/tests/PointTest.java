83ecd1f8dd392b6ed70ab45db241676e1efc08fe
loicbefve <befve.loic@gmail.com>
2017-11-12 14:07:31 +0100

    Rajout de la classe de test pour la classe kdtree.Point

---
+++ src/tests/PointTest.java
@@ -0,0 +1,54 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PointTest {
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCoord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCoords() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCoord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOneCoord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDistsq() {
+		fail("Not yet implemented");
+	}
+
+}
