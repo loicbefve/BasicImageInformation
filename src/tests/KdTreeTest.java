09de23b64f41bba26ec659e2ac4b0980715888f0
loicbefve <befve.loic@gmail.com>
2017-11-12 14:01:53 +0100

    Mise en place de l'architecture des tests

---
+++ src/tests/KdTreeTest.java
@@ -0,0 +1,44 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KdTreeTest {
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKdTree() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRecherche() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddNode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNearestNeighbor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTroncature() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetColors() {
+		fail("Not yet implemented");
+	}
+
+}
