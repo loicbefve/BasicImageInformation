6d7b731ce5b1f815f10cbc37e29cb98f26770117
loicbefve <befve.loic@gmail.com>
2017-11-12 16:49:18 +0100

    Implémentation de testGetCoords et testSetCoord + utilisation de la méthode setUp

--- src/tests/PointTest.java
+++ src/tests/PointTest.java
@@ -11,7 +11,11 @@ import org.junit.Test;
 import kdtree.Point;
 
 public class PointTest {
-
+	
+	private static Point testPoint;
+	private static int listeTestPoint[] = {0,1000000,-1000000,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1};
+	private static int sizeTestPoint = listeTestPoint.length;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -22,6 +26,7 @@ public class PointTest {
 
 	@Before
 	public void setUp() throws Exception {
+		testPoint = new Point( listeTestPoint );
 	}
 
 	@After
@@ -45,28 +50,31 @@ public class PointTest {
 
 	@Test
 	public void testGetCoord() {
-		Point TestPoint = new Point( 0 ,  1000000 , -1000000 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 );
-		assertTrue("Zéro" , TestPoint.getCoord(0) == 0);
-		assertTrue("1000000" , TestPoint.getCoord(1) == 1000000);
-		assertTrue("-1000000" , TestPoint.getCoord(2) == -1000000);
-		assertTrue("Grande dimension" , TestPoint.getCoord(15) == 13);
-		
-		Point TestPointOne = new Point(1);
-		assertTrue("Point à une seul coordonée" , TestPointOne.getCoord(0) == 1);
+		assertTrue("Zéro" , testPoint.getCoord(0) == listeTestPoint[0]);
+		assertTrue("1000000" , testPoint.getCoord(1) == listeTestPoint[1]);
+		assertTrue("-1000000" , testPoint.getCoord(2) == listeTestPoint[2]);
+		assertTrue("Grande dimension" , testPoint.getCoord(sizeTestPoint-1) == listeTestPoint[sizeTestPoint-1]);
 	}
 	
-	
-
 	@Test
 	public void testGetCoords() {
-		Point TestPoint = new Point( 0 ,  1000000 , -1000000 );
-		int[] res = {0 , 1000000 , - 1000000};
-		assertTrue("Point = [0 , 1000000 , -1000000]" , TestPoint.getCoords() == res);
+		int resTrouvee[] = testPoint.getCoords();
+		for( int i = 0 ; i<sizeTestPoint ; i++) {
+			assertTrue("Egalité resTheo et resTrouvee" , resTrouvee[i] == listeTestPoint[i]);
+		}
 	}
 
 	@Test
 	public void testSetCoord() {
-		fail("Not yet implemented");
+		int listeNewPoint[] = {100,1,-1000000,1,2,3,-4,5,6,7,-8,9,1,2,3,-4,5,6,5787,8,9,1,92,3,4,5698,6,7,68,9,100000};
+		int sizeListeNewPoint = listeNewPoint.length;
+		
+		testPoint.setCoord(listeNewPoint);
+		int resTrouvee[] = testPoint.getCoords();
+		
+		for( int i = 0 ; i<sizeListeNewPoint ; i++) {
+			assertTrue("Egalité resTheo et resTrouvee" , resTrouvee[i] == listeNewPoint[i]);
+		}
 	}
 
 	@Test
